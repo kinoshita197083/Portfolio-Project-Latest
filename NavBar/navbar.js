93e1956facbda60fe8347011be5a2789ae8425f3
kinoshita197083 <83145802+kinoshita197083@users.noreply.github.com>
2023-03-02 22:29:10 +1100

    skeleton_completed

--- NavBar/navbar.js
+++ NavBar/navbar.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector('.menu-toggle'),
     mobileMenu = document.querySelector('.nav-menu'),
-    navLinks = document.querySelectorAll('.nav-links');
+    navLinks = document.querySelectorAll('.nav-links'),
+    navContainer = document.querySelector('.nav-container');
 
 //OPEN or CLOSE the mobile menu
 const toggleMenu = () => {
@@ -14,4 +15,12 @@ menuIcon.addEventListener('click', () => {
 
 navLinks.forEach(el => {
     el.addEventListener('click', toggleMenu)
+})
+
+document.addEventListener('scroll', () => {
+    if (scrollY > 100) {
+        navContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+    } else {
+        navContainer.style.backgroundColor = 'transparent'
+    }
 })
