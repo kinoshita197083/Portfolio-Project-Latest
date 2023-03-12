084aa3ee4e4729d905f34891f51469a1a234450c
kinoshita197083 <83145802+kinoshita197083@users.noreply.github.com>
2023-03-12 17:03:09 +1100

    iteration_2_project_page_added

---
+++ app.js
@@ -0,0 +1,49 @@
+//Regular Element Selecting
+const fullScreenElement = document.querySelector('#full-screen-img-holder'),
+    images = document.querySelectorAll('img');
+
+//Fadein Animation & Lazy Loading
+const fadeInElements = document.querySelectorAll('.fade-in'),
+    HomeFadeIn = document.querySelectorAll('.home-fade-in');
+
+images.forEach(img => {
+    img.addEventListener('click', enlargeImage)
+})
+
+//Render the image in full screen
+function enlargeImage() {
+    if (this) {
+        let src = this.src;
+
+        let imgElement = document.createElement('img');
+        imgElement.classList.add('full-screen-img');
+        imgElement.src = src;
+
+        fullScreenElement.appendChild(imgElement);
+        fullScreenElement.classList.add('active');
+    }
+}
+
+//Click and close the full screen image
+fullScreenElement.addEventListener('click', () => {
+    fullScreenElement.classList.remove('active');
+    fullScreenElement.removeChild(fullScreenElement.lastChild)
+})
+
+
+// reference: https://www.youtube.com/watch?v=T33NN_pPeNI&t=19s&ab_channel=BeyondFireship
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+});
+
+fadeInElements.forEach(el => {
+    observer.observe(el);
+})
+
+HomeFadeIn.forEach(el => {
+    observer.observe(el);
+}) 
